2149641a4eb2117bf8683a2b1ceaae5609661cd0
Klestra <klestra@protonmail.com>
2018-05-27 16:42:00 +0200

    Added subject variable

--- email.php
+++ email.php
@@ -2,9 +2,10 @@
     $to = 'corre.mathieu@gmail.com';
     $name = $_POST['firstName'];
     $last = $_POST['lastName'];
+    $subject = $_POST['subject'];
     $message = $_POST['text'] . "\r\n" .  $_POST['email'];
     $headers = 'From: ' . $_POST['email'] . "\r\n" .
      'Reply-To: ' . $_POST['email'] . "\r\n" .
      'X-Mailer: PHP/' . phpversion();
-    mail($to, $name, $last, $message, $headers);
+    mail($to, $name, $last, $subject, $message, $headers);
     header('Location: /index.html');
