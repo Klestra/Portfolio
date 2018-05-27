d21265a4df3fac75202fbba72573bc595aef6d87
Klestra <klestra@protonmail.com>
2018-05-27 15:58:57 +0200

    Added php form fonctionality

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php
+    $to = 'corre.mathieu@gmail.com';
+    $name = $_POST['firstName'];
+    $last = $_POST['lastName'];
+    $subject = $_POST['subject'];
+    $message = $_POST['text'] . "\r\n" .  $_POST['email'];
+    $headers = 'From: ' . $_POST['email'] . "\r\n" .
+     'Reply-To: ' . $_POST['email'] . "\r\n" .
+     'X-Mailer: PHP/' . phpversion();
+    mail($to, $name, $last, $suject, $message, $headers);
+    header('Location: /index.html');
