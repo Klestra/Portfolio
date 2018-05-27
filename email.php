814641d368b27e7933b3f9605db7f458bfe87eed
Klestra <klestra@protonmail.com>
2018-05-27 16:03:35 +0200

    Resolved somes bugs

--- email.php
+++ email.php
@@ -2,10 +2,9 @@
     $to = 'corre.mathieu@gmail.com';
     $name = $_POST['firstName'];
     $last = $_POST['lastName'];
-    $subject = $_POST['subject'];
     $message = $_POST['text'] . "\r\n" .  $_POST['email'];
     $headers = 'From: ' . $_POST['email'] . "\r\n" .
      'Reply-To: ' . $_POST['email'] . "\r\n" .
      'X-Mailer: PHP/' . phpversion();
-    mail($to, $name, $last, $suject, $message, $headers);
+    mail($to, $name, $last, $message, $headers);
     header('Location: /index.html');
